e86a25b266aa975ccc6608998df32316e2f73343
RaviRajGhorsai <graviraj344@gmail.com>
2025-05-09 15:47:35 +0545

    login bypass fixed

--- Project/app/views.py
+++ Project/app/views.py
@@ -12,7 +12,6 @@ from datetime import datetime, timedelta
 import hashlib
 from utils import send_register_mail, send_otp_mail, verify_mail
 from .models import Group, GroupMessage
-
 from .forms import chatMessageForm
 
 
@@ -68,7 +67,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
-            login(request, user)
+            
             otp = ''.join(str(secrets.randbelow(10)) for _ in range(6))
             request.session['otp'] = hashlib.sha256(otp.encode()).hexdigest()
             request.session['user_id'] = user.id
@@ -108,11 +107,14 @@ def otp_verify(request):
             else:
                 
                 user = User.objects.get(id=user_id)
-                login(request, user)
-                # Clear the OTP from the session after successful verification
                 request.session.pop('otp', None)
                 request.session.pop('user_id', None)
                 request.session.pop('otp-created-at', None)
+                request.session.delete()
+                
+                
+                login(request, user)
+                request.session['otp_verified'] = True
                 return redirect('dashboard')
         else:
             
@@ -122,108 +124,117 @@ def otp_verify(request):
 @login_required
 def dashboard(request):
     if request.user.is_authenticated == True:
-       print("hello from dashboard")
-       return render(request, 'chat/base.html')
-    else:
-        print("User is not authenticated")
-        return redirect('login')  # Redirect to the login page if not authenticated
+        if request.session.get('otp_verified') == True:
+            print("hello from dashboard")
+            return render(request, 'chat/base.html')
+        else:
+            print("User is not authenticated")
+            
+            return redirect('login')  # Redirect to the login page if not authenticated
     
 @login_required   
 def profile_view(request):
     if request.user.is_authenticated:
-        return render(request, 'chat/profile.html')
+        if request.session.get('otp_verified') == True:
+            return render(request, 'chat/profile.html')
 
 @login_required
 def create_group_view(request):
     if request.user.is_authenticated == True:
-        if request.method == 'POST':
-            # Handle the group creation logic 
-            group_name = request.POST.get('group_name', '').strip()
-            group_key = request.POST.get('group_key', '').strip()
+        if request.session.get('otp_verified') == True:
+            if request.method == 'POST':
+                # Handle the group creation logic 
+                group_name = request.POST.get('group_name', '').strip()
+                group_key = request.POST.get('group_key', '').strip()
+                
+                if not group_name or not group_key:
+                    print("All fields are required")
+                    return render(request, 'chat/create_group.html', {'error': 'All fields are required'})
+                
+                if Group.objects.filter(name=group_name).exists():
+                    print("Group name already exists")
+                    return render(request, 'chat/create_group.html', {'error': 'Group name already exists'})
+                
+                group = Group.objects.create(name=group_name, key=group_key)
+                group.members.add(request.user)
+                group.save()
+                if group:
+                    return redirect('chat')
+                else:
+                    print("Failed to create group")
+                    return render(request, 'chat/create_group.html', {'error': 'Failed to create group'})
+                
+            return render(request, 'chat/create_group.html')
+        else:
             
-            if not group_name or not group_key:
-                print("All fields are required")
-                return render(request, 'chat/create_group.html', {'error': 'All fields are required'})
-            
-            if Group.objects.filter(name=group_name).exists():
-                print("Group name already exists")
-                return render(request, 'chat/create_group.html', {'error': 'Group name already exists'})
-            
-            group = Group.objects.create(name=group_name, key=group_key)
-            group.members.add(request.user)
-            group.save()
-            if group:
-                return redirect('chat')
-            else:
-                print("Failed to create group")
-                return render(request, 'chat/create_group.html', {'error': 'Failed to create group'})
-            
-        return render(request, 'chat/create_group.html')
-    else:
-        return redirect('login')
+            return redirect('login')
 
 @login_required
 def join_group_view(request):
     if request.user.is_authenticated == True:
-        if request.method == 'POST':
-            # Handle the group joining logic 
-            group_key = request.POST.get('groupKey', '').strip()
-            group_name = request.POST.get('groupName', '').strip()
-            
-            if not group_key or not group_name:
-                print("Group key is required")
-                return render(request, 'chat/join_group.html', {'error': 'Group name and key is required'})
-    
-            try:
-                group = Group.objects.get(name= group_name, key=group_key)
-            except Group.DoesNotExist:
-                print("Group does not exist")
-                return render(request, 'chat/join_group.html', {'error': 'Group does not exist'})
-            
-            if request.user not in group.members.all():
-                group.members.add(request.user)
-                group.save()
-                messages.success(request, f"You have successfully joined the group {group.name}.")
-                print(f"You have successfully joined the group {group.name}.")
-                return redirect('chat')
-            else:
-                print("You are already a member of this group")
+        if request.session.get('otp_verified') == True:
+            if request.method == 'POST':
+                # Handle the group joining logic 
+                group_key = request.POST.get('groupKey', '').strip()
+                group_name = request.POST.get('groupName', '').strip()
                 
+                if not group_key or not group_name:
+                    print("Group key is required")
+                    return render(request, 'chat/join_group.html', {'error': 'Group name and key is required'})
+        
+                try:
+                    group = Group.objects.get(name= group_name, key=group_key)
+                except Group.DoesNotExist:
+                    print("Group does not exist")
+                    return render(request, 'chat/join_group.html', {'error': 'Group does not exist'})
                 
-                return redirect('chat')
-        return render(request, 'chat/join_group.html')
-    else:
-        return redirect('login')
+                if request.user not in group.members.all():
+                    group.members.add(request.user)
+                    group.save()
+                    messages.success(request, f"You have successfully joined the group {group.name}.")
+                    print(f"You have successfully joined the group {group.name}.")
+                    return redirect('chat')
+                else:
+                    print("You are already a member of this group")
+                    
+                    
+                    return redirect('chat')
+            return render(request, 'chat/join_group.html')
+        else:
+            
+            return redirect('login')
 
 @login_required
 def chat_view(request, chat_room_name="Public"):
     if request.user.is_authenticated == True:
-        chat_group = get_object_or_404(Group, name=chat_room_name)
-        chat_messages = chat_group.messages.order_by('-created')[:30][::-1]
-        form = chatMessageForm()
-        
-        if request.htmx:
-            form = chatMessageForm(request.POST)
-            if form.is_valid():
-                message = form.save(commit=False)
-                message.user = request.user
-                message.group = chat_group
-                message.save()
-                context = {
-                    'message': message,
-                    'user': request.user,
-                }
-                return render(request, 'chat/partials/chat_message_p.html', context)
-        
-        context = {
-            'chat_groups': chat_group,
-            'chat_messages': chat_messages,
-            'chat_room_name': chat_room_name,
-            'form': form,   
-        }
-        return render(request, 'chat/chat.html', context)
-    else:
-        return redirect('login')
+        if request.session.get('otp_verified') == True:
+            chat_group = get_object_or_404(Group, name=chat_room_name)
+            chat_messages = chat_group.messages.order_by('-created')[:30][::-1]
+            form = chatMessageForm()
+            
+            if request.htmx:
+                form = chatMessageForm(request.POST)
+                if form.is_valid():
+                    message = form.save(commit=False)
+                    message.user = request.user
+                    message.group = chat_group
+                    message.save()
+                    context = {
+                        'message': message,
+                        'user': request.user,
+                    }
+                    return render(request, 'chat/partials/chat_message_p.html', context)
+            
+            context = {
+                'chat_groups': chat_group,
+                'chat_messages': chat_messages,
+                'chat_room_name': chat_room_name,
+                'form': form,   
+            }
+            return render(request, 'chat/chat.html', context)
+        else:
+            
+            return redirect('login')
 
 @login_required
 def logout_view(request):
